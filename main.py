0c1d1ba7fcde40c8ca5f0b232efc0b877b0eda85
olenalipinska <eveforelinska@gmail.com>
2023-10-08 22:44:23 +0300

    Lucky ticket app create, Thread add

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from threading import Thread
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def generate_tickets(tickets_number, digits_in_ticket):
+    return [f"%.{digits_in_ticket}d" % i for i in range(tickets_number)]
 
+def get_ticket_groups(tickets):
+    return [tickets[x:x + len(tickets) // 4] for x in range(0, len(tickets), len(tickets) // 4)]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_lucky_ticket(group):
+    for i in group:
+        i = list(map(int, i))
+        if i[0]+i[1]+i[2]==i[3]+i[4]+i[5]:
+            lucky_tickets.append(i)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    lucky_tickets = []
+    tickets = generate_tickets(1000000, 6)
+    ticket_groups = get_ticket_groups(tickets)
+    threads = []
+    for group in ticket_groups:
+        threads.append(Thread(target=get_lucky_ticket, args=[group]))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    print(len(lucky_tickets))
