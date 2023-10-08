608e3c82a14c8cd33b5e337f1d29df8300fd51b8
olenalipinska <eveforelinska@gmail.com>
2023-10-08 22:55:42 +0300

    Lucky ticket app create, Thread add

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ def get_lucky_ticket(group):
         if i[0]+i[1]+i[2]==i[3]+i[4]+i[5]:
             lucky_tickets.append(i)
 
-if __name__ == '__main__':
-    lucky_tickets = []
-    tickets = generate_tickets(1000000, 6)
-    ticket_groups = get_ticket_groups(tickets)
+def main(ticket_groups):
     threads = []
     for group in ticket_groups:
         threads.append(Thread(target=get_lucky_ticket, args=[group]))
@@ -24,3 +21,9 @@ if __name__ == '__main__':
     for t in threads:
         t.join()
     print(len(lucky_tickets))
+
+if __name__ == '__main__':
+    lucky_tickets = []
+    tickets = generate_tickets(1000000, 6)
+    ticket_groups = get_ticket_groups(tickets)
+    main(ticket_groups)
